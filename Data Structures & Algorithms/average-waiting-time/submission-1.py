f709a724bb85fd150f7764f0c5955e26e2c3423f
Léo <chabriertaillantleopold@gmail.com>
2026-09-22 19:08:46 -0400

    Add: average-waiting-time - submission-1

---
+++ Data Structures & Algorithms/average-waiting-time/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        last = 1
+        tmp_sum = 0
+
+        for arrival, time in customers:
+            
+            if arrival > last:
+                tmp_sum += time
+                last = arrival+time
+            else:
+                tmp_sum += time+last-arrival
+                last += time
+            
+        return tmp_sum/len(customers)
